0eabe8edef7e16ce4dfb14727012297c315a6a40
Feilan Jiang <fjiang120@gmail.com>
2018-02-03 18:02:53 -0500

    create api endpoints and their request handlers

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.post('/imageToFlags', function(req, res) {
+  console.log('image to flags request');
+  res.send('image to flags request');
+});
+
+app.post('/imageToIngredients', function(req, res) {
+  console.log('image to ingredients request');
+  res.send('image to ingredients request');
+});
+
+app.get('/ingredientsToFlags', function(req, res) {
+  console.log('ingredients to flags request');
+  res.send('ingredients to flags request');
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
+
