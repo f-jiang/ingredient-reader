2aaeaa87c21bbe57a92791d1f5d2b16c176373e7
Feilan Jiang <fjiang120@gmail.com>
2018-02-03 23:37:44 -0500

    add azure ocr api call for /imageToIngredients

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const ingReader = require('./models/ingredientReader.js');
 const bodyParser = require('body-parser');
+const request = require('request');
+const querystring = require('querystring');
+const dotenv = require('dotenv');
 
 const app = express();
 
-app.post('/imageToFlags', function(req, res) {
+dotenv.config();
+
+const subscriptionKey = process.env.API_KEY;
+
+app.get('/imageToFlags', function(req, res) {
   console.log(res);
   res.send('image to flags request');
 });
 
-app.post('/imageToIngredients', function(req, res) {
-  console.log('image to ingredients request');
-  res.send('image to ingredients request');
+app.get('/imageToIngredients', function(req, res) {
+  if (req.query.imgUrl) {
+    var imgUrl = req.query.imgUrl;
+
+    request.post({
+      // TODO add a params object and build the query string separately
+      url: 'https://eastus.api.cognitive.microsoft.com/vision/v1.0/ocr?language=unk&detectOrientation=true',
+      body: "{'url': '" + imgUrl + "'}",
+      headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscriptionKey
+      }
+    }, function(error, response, body) {
+      res.send(JSON.parse(body));
+    });
+  }
 });
 
 app.get('/ingredientsToFlags', function(req, res) {
